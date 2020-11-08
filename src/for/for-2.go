a3e3a5dd273964490f65e2cb89bd2ea2919bccec
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2020-11-08 11:42:13 +0530

    main

--- src/for/for-2.go
+++ src/for/for-2.go
@@ -1,14 +1,15 @@
 package main
+
 import "fmt"
 
-func main(){
-var a=17
-for i:=2;i<a;i++{
-if a%i==0{
-fmt.Printf("not prime\n")
-break
-}else if i==a-1{
-fmt.Printf("PRIME \n")
-}
-}
+func main() {
+	var a = 17
+	for i := 2; i < a; i++ {
+		if a%i == 0 {
+			fmt.Printf("not prime\n")
+			break
+		} else if i == a-1 {
+			fmt.Printf("PRIME \n")
+		}
+	}
 }
