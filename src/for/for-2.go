33dbc605ce130db70279c666325c3c76f0b71d73
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2020-11-08 11:43:30 +0530

    main

--- src/for/for-2.go
+++ src/for/for-2.go
@@ -6,10 +6,10 @@ func main() {
 	var a = 17
 	for i := 2; i < a; i++ {
 		if a%i == 0 {
-			fmt.Printf("not prime\n")
+			fmt.Printf("not  a prime\n")
 			break
 		} else if i == a-1 {
-			fmt.Printf("PRIME \n")
+			fmt.Printf(" a PRIME \n")
 		}
 	}
 }
