3e1944db7a21e4116261f633edaf0a73281229c2
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2020-11-23 20:50:54 +0530

    main

--- src/tcpp3.go
+++ src/tcpp3.go
@@ -17,6 +17,9 @@ func main() {
 	var CONN_PORT string
 	fmt.Println("Enter the port")
 	fmt.Scanf("%s", &CONN_PORT)
+	var CONN_MSG string
+	fmt.Println("Enter the message")
+	fmt.Scanf("%s", &CONN_MSG)
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -32,9 +35,7 @@ func main() {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		var CONN_MSG string
-		fmt.Println("Enter the message")
-		fmt.Scanf("%s", &CONN_MSG)
+
 		// Handle connections in a new goroutine.
 		go handleRequest(conn, CONN_MSG)
 	}
@@ -43,25 +44,28 @@ func main() {
 // Handles incoming requests.
 func handleRequest(conn net.Conn, msg string) {
 	// Make a buffer to hold incoming data.
-	buf := make([]byte, 1024)
+	buf := make([]byte, 8192)
 	// Read the incoming connection into the buffer.
 	_, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
 	// Send a response back to person contacting us.
-	conn.Write([]byte("Message receivedZ."))
+	conn.Write([]byte("Message received."))
 	conn.Write([]byte(msg)) //
 	// Close the connection when you're done with it.
 	conn.Close()
 }
 
 //execution steps
-// step-1
-// durgaprasad@durgaprasad-Lenovo-E49:~/go/golang/src$ go build tcpp3.go
-// durgaprasad@durgaprasad-Lenovo-E49:~/go/golang/src$ ./tcpp3
+// // step-1
+// durgaprasad@durgaprasad-Lenovo-E49:~/go/golang/src$ go run tcpp3.go
+// Enter the port
+// 3333
+// Enter the message
+// durgaprasad
 // Listening on localhost:3333
-
-// step-2
-// durgaprasad@durgaprasad-Lenovo-E49:~/go/golang$ echo -n "test server" |nc localhost 3333
-// Message received.
+// // step-2
+// open another bash
+// durgaprasad@durgaprasad-Lenovo-E49:~/go/golang$ echo -n "test"|nc localhost 3333
+// Message received.durgaprasad
