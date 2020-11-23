1057f1f7a1930a844c3df4e4d6286b91549e9fca
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2020-11-23 20:30:13 +0530

    main

--- src/tcpp3.go
+++ src/tcpp3.go
@@ -32,13 +32,16 @@ func main() {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
+		var CONN_MSG string
+		fmt.Println("Enter the message")
+		fmt.Scanf("%s", &CONN_MSG)
 		// Handle connections in a new goroutine.
-		go handleRequest(conn)
+		go handleRequest(conn, CONN_MSG)
 	}
 }
 
 // Handles incoming requests.
-func handleRequest(conn net.Conn) {
+func handleRequest(conn net.Conn, msg string) {
 	// Make a buffer to hold incoming data.
 	buf := make([]byte, 1024)
 	// Read the incoming connection into the buffer.
@@ -47,7 +50,8 @@ func handleRequest(conn net.Conn) {
 		fmt.Println("Error reading:", err.Error())
 	}
 	// Send a response back to person contacting us.
-	conn.Write([]byte("Message received."))
+	conn.Write([]byte("Message receivedZ."))
+	conn.Write([]byte(msg)) //
 	// Close the connection when you're done with it.
 	conn.Close()
 }
