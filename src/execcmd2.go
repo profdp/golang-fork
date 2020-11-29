36cc9bf75b3386bf1cb3833bf7e22830239719b5
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2020-11-29 22:24:22 +0530

    main

--- src/execcmd2.go
+++ src/execcmd2.go
@@ -11,13 +11,20 @@ func main() {
 	grepCmd := exec.Command("grep", "durga")
 
 	grepIn, _ := grepCmd.StdinPipe()
+
 	grepOut, _ := grepCmd.StdoutPipe()
+
 	grepCmd.Start()
+
 	grepIn.Write([]byte("hello grep\ngoodbye durga grep"))
+
 	grepIn.Close()
+
 	grepBytes, _ := ioutil.ReadAll(grepOut)
+
 	grepCmd.Wait()
 
 	fmt.Println("> grep durga")
+
 	fmt.Println(string(grepBytes))
 }
