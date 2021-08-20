81792d695612dfd22ca2ad5a46fee4adeb47f8b6
“durgaprasad-palanati-AI” <dp.cse5@gmail.com>
2021-08-20 23:54:56 +0530

    hash with name+ctime

--- src/BLOCKCHAIN/block2.go
+++ src/BLOCKCHAIN/block2.go
@@ -1,4 +1,5 @@
 //my blockchain program-2
+//hashing name+ctime
 package main
 
 import (
@@ -18,19 +19,19 @@ func main() {
 	genesisTransactions := []string{"50", "30"}
 	var name = "GT"
 	genesisBlock := NewBlock(genesisTransactions, []byte{}, name)
-	fmt.Println("--- First Block ---")
+	fmt.Println("--- First Block ---" + name)
 	printBlockInformation(genesisBlock)
 
 	block2Transactions := []string{"40", "30"}
 	name = "B2T"
 	block2 := NewBlock(block2Transactions, genesisBlock.Hash, name)
-	fmt.Println("--- Second Block ---")
+	fmt.Println("--- Second Block ---" + name)
 	printBlockInformation(block2)
 
 	block3Transactions := []string{"30", "30"}
 	name = "B3T"
 	block3 := NewBlock(block3Transactions, block2.Hash, name)
-	fmt.Println("--- Third Block ---")
+	fmt.Println("--- Third Block ---" + name)
 	printBlockInformation(block3)
 }
 
@@ -49,7 +50,7 @@ func NewHash(time time.Time, transactions []string, prevHash []byte, name string
 	input := append(prevHash, time.String()...)
 	for transaction := range transactions {
 		input = append(input, string(rune(transaction))...)
-		input = append(input, name...)
+		input = append(input, name...) //append name
 	}
 	hash := sha256.Sum256(input)
 	return hash[:]
