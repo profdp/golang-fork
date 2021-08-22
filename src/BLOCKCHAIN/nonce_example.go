5245c547892e9ca7d0ad8658654bd146dbdb5337
“durgaprasad-palanati-AI” <dp.cse5@gmail.com>
2021-08-22 18:05:44 +0530

    basic nonce_creation-example in GOLANG

---
+++ src/BLOCKCHAIN/nonce_example.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	data := []byte("12")
+	//hash of string
+	hash := sha256.Sum256(data)
+	fmt.Println(hash[0])
+	//hash of integer
+	n := 25
+	nb := []byte(strconv.Itoa(n))
+	hash1 := sha256.Sum256(nb)
+	fmt.Println(hash1[0])
+	//checking for target value
+	target := 222
+	if int(hash1[0]) > target {
+		fmt.Println("yes grater than target")
+	} else {
+		fmt.Println("not grater than target")
+	}
+	//****************************
+	hash2 := sha256.Sum256(append(data, nb...))
+
+	fmt.Println("hash2[0]=", hash2[0])
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("target= ", target)
+	fmt.Println((int(hash2[0]) > target))
+	//repeat for nonce
+	for i := true; i; i = (int(hash2[0]) > target) {
+		n = n + 1
+		nbn := []byte(strconv.Itoa(n))
+		hash2 = sha256.Sum256(append(data, nbn...))
+		fmt.Println("n=", n)
+		fmt.Println("hash2[0]=", hash2[0])
+		time.Sleep(3000 * time.Millisecond)
+	}
+	fmt.Println("nonce=", n)
+}
