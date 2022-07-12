a05ec99a38d6522a42fdba4858abdff9b20ac0fb
durgaprasad-palanati-AI <dp.cse5@gmail.com>
2022-07-12 08:25:13 +0530

    nonce_example is modified

--- src/BLOCKCHAIN/nonce_example.go
+++ src/BLOCKCHAIN/nonce_example.go
@@ -8,15 +8,18 @@ import (
 )
 
 func main() {
-	data := []byte("12")
+	data := []byte("11")
 	//hash of string
 	hash := sha256.Sum256(data)
+	fmt.Println("data=", data)
+	fmt.Println("hash=", hash)
 	fmt.Println(hash[0])
 	//hash of integer
 	n := 25
 	nb := []byte(strconv.Itoa(n))
+	fmt.Println("nb=", nb)
 	hash1 := sha256.Sum256(nb)
-	fmt.Println(hash1[0])
+	fmt.Println("hash1[0]=", hash1[0])
 	//checking for target value
 	target := 222
 	if int(hash1[0]) > target {
@@ -35,6 +38,7 @@ func main() {
 	for i := true; i; i = (int(hash2[0]) > target) {
 		n = n + 1
 		nbn := []byte(strconv.Itoa(n))
+		fmt.Println("nbn=", nbn)
 		hash2 = sha256.Sum256(append(data, nbn...))
 		fmt.Println("n=", n)
 		fmt.Println("hash2[0]=", hash2[0])
